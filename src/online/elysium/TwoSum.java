54dd0c718859c2428bded7ccb6886ed8a19c972d
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-04 20:17:33 +0800

    u

---
+++ src/online/elysium/TwoSum.java
@@ -0,0 +1,20 @@
+package online.elysium;
+
+class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null) {
+            return null;
+        }
+
+        int length = nums.length;
+        if (length < 2) {
+            return null;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        return new int[]{0, 1};
+    }
+}

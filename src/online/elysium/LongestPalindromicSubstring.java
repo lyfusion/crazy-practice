0a6fdfdd2bec1355eed8b72b512cf47ab8fe02cb
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-06 17:54:36 +0800

    u

---
+++ src/online/elysium/LongestPalindromicSubstring.java
@@ -0,0 +1,21 @@
+package online.elysium;
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        StringBuilder result = new StringBuilder();
+
+        // s.length > 2
+        // babad
+        int left = 0;
+        int right = s.length() - 1;
+        
+
+        // cbbd
+
+        // 
+        
+
+
+        return result.toString();
+    }
+}

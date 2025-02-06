62757fc721e3d79c2a5c1528b3ff35231751c273
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-06 18:09:00 +0800

    u

--- src/online/elysium/LongestPalindromicSubstring.java
+++ src/online/elysium/LongestPalindromicSubstring.java
@@ -2,20 +2,29 @@ package online.elysium;
 
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
-        StringBuilder result = new StringBuilder();
 
-        // s.length > 2
         // babad
-        int left = 0;
-        int right = s.length() - 1;
-        
-
         // cbbd
-
-        // 
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            int size = chars.length - i;
+            for (int j = 0; j < i + 1; j++) {
+                if (isPalindrome(s.substring(j, j + size))) {
+                    return s.substring(j, j + size);
+                }
+            }
+        }
         
+        return "";
+    }
 
+    private boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
 
-        return result.toString();
+        return true;
     }
 }

f98472f1118556a1ff9e3aa00f85fb63c3b6dc06
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-07 14:12:23 +0800

    Fix bug test case x=10

--- src/online/elysium/PalindromeNumber.java
+++ src/online/elysium/PalindromeNumber.java
@@ -2,7 +2,7 @@ package online.elysium;
 
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
 

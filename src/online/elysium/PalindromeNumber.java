2252ada4d44916bf49ea28a11372f37f6107d0a6
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-07 13:50:36 +0800

    narrow the traversal range

--- src/online/elysium/PalindromeNumber.java
+++ src/online/elysium/PalindromeNumber.java
@@ -7,7 +7,7 @@ public class PalindromeNumber {
         }
 
         String s = String.valueOf(x);
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length() / 2; i++) {
             if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
             }

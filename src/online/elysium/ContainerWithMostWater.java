61dd4ed559161b72340d4d0304706f910358ca0e
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-08 16:25:38 +0800

    Original solution

--- src/online/elysium/ContainerWithMostWater.java
+++ src/online/elysium/ContainerWithMostWater.java
@@ -3,8 +3,17 @@ package online.elysium;
 class ContainerWithMostWater {
 
     public int maxArea(int[] height) {
+        int maxArea = 0;
         //[i, j] area = (j - i) * min(height[i], height[j])
+        for (int i = 1; i < height.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int w = i - j;
+                int h = Math.min(height[i], height[j]);
+                int area = w * h;
+                maxArea = Math.max(maxArea, area);
+            }
+        }
 
-        return 0;
+        return maxArea;
     }
 }

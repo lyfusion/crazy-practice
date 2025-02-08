da84a40cdf99fb8e21509226b996f6b354039ffe
Frank Liu <frankliuyang.ly@gmail.com>
2025-02-08 15:08:23 +0800

    Container with most water

---
+++ src/online/elysium/ContainerWithMostWater.java
@@ -0,0 +1,10 @@
+package online.elysium;
+
+class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        //[i, j] area = (j - i) * min(height[i], height[j])
+
+        return 0;
+    }
+}

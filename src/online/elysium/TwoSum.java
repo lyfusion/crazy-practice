1f873e96732a6b7ed4e8c6deb5462a2a92abaf3a
Yang Liu <frankliuyang@vip.163.com>
2025-04-15 18:30:27 +0800

    :bug: Fix

--- src/online/elysium/TwoSum.java
+++ src/online/elysium/TwoSum.java
@@ -20,4 +20,16 @@ class TwoSum {
 
         return null;
     }
+
+    public int[] twoSum1(int[] nums, int target) {
+        int length = nums.length - 1;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j <= length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                } 
+            }
+        }
+        return null;
+    }
 }
